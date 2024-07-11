97fb5656618617e2a99a6b2ede8dec9f20003fed
neprostostas <neprostostas.gg@gmail.com>
2024-07-11 20:17:11 +0300

    adaptive content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", (event) => {
 
+    const stars = document.querySelectorAll(".star-img");
 
+    const toggleHighlight = (index) => {
+        stars.forEach((star, i) => star.classList.toggle("hovered", i <= index));
+    };
 
-})
+    stars.forEach((star, index) => {
+        star.addEventListener("mouseover", () => toggleHighlight(index));
+        star.addEventListener("mouseout", () => toggleHighlight(-1));
+    });
+
+});
