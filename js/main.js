14987c1a0a1c263457c5a2ef84a0b9d57ed48f73
neprostostas <neprostostas.gg@gmail.com>
2024-07-11 22:04:29 +0300

    added scrolling table-content descktop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
+    /* ======== stars hovering ======== */
     const stars = document.querySelectorAll(".star-img");
 
     const toggleHighlight = (index) => {
@@ -10,5 +11,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
         star.addEventListener("mouseover", () => toggleHighlight(index));
         star.addEventListener("mouseout", () => toggleHighlight(-1));
     });
+
+    /* ======== scrolling table-content ======== */
+    const sections = [...document.querySelectorAll('.table-content')];
+    const listItems = [...document.querySelectorAll('.table-content-point')];
+    // Завжди додаємо клас .pass до першого пункту
+    listItems[0].classList.add('pass');
+
+
+    document.addEventListener('scroll', () => {
+
+        // Знайти поточний індекс секції
+        let currentSectionIndex = sections.findIndex(section => section.getBoundingClientRect().top > window.innerHeight) - 1;
+
+        // Якщо вікно прокручено до кінця сторінки або останній елемент видно, встановити currentSectionIndex на останній елемент
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight ||
+            sections[sections.length - 1].getBoundingClientRect().top <= window.innerHeight) {
+            currentSectionIndex = sections.length - 1;
+        }
+
+        listItems.forEach((item, index) => {
+            if (index <= currentSectionIndex) {
+                item.classList.add('pass');
+            } else {
+                item.classList.remove('pass');
+            }
+        });
+    });
+
+
+
+
+
 
 });
